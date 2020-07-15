189efa486b5609f6ed646f8219d794628bcf5c77
rabah babaci <4rabah@gmail.com>
2020-07-14 17:54:24 -0700

    add the getNextRoom method to the Room class

--- src/room.py
+++ src/room.py
@@ -4,3 +4,13 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def getNextRoom(self, userInput):
+        if userInput == "n":
+            return self.n_to
+        elif userInput == "s":
+            return self.s_to
+        elif userInput == "e":
+            return self.e_to
+        else:
+            return self.w_to

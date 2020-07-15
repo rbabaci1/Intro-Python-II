126ea1840388e7f212ef9be963c0bd1f43273eba
rabah babaci <4rabah@gmail.com>
2020-07-15 11:39:23 -0700

    add the directions attrs to the rooms

--- src/room.py
+++ src/room.py
@@ -2,6 +2,10 @@ class Room:
     def __init__(self, name, description, list=None):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
         if list is None:
             self.list = []
         else:
